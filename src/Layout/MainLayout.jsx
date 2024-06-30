595017918ef87dcd334f9886e1ebf05338986903
sujanchandrar <roys33664@gmail.com>
2024-06-30 21:54:20 +0600

    add a new footer section

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
 
+
 const MainLayout = () => {
     return (
         <div>
             <Outlet></Outlet>
-            <Footer></Footer>
+           <Footer></Footer>
+           
         </div>
     );
 };
